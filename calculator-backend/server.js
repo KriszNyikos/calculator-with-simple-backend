b0af21876a7ea04f4615a09e69ace90401f07133
KriszNyikos <krisz.nyikos@gmail.com>
2023-07-08 12:36:28 +0200

    add basic API logic

--- calculator-backend/server.js
+++ calculator-backend/server.js
@@ -1,9 +1,33 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 const port = 3001
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/number', (req, res) => {
+  fs.readFile('./data/numberData.json', 'utf-8', (err, data) => {
+    if (err) throw err
+    res.send(data)
+})
+})
+
+app.post('/number', (req, res) => {
+const { number } = req.body
+
+const updateData = JSON.stringify({"storedNumber": number}) 
+
+
+ fs.writeFile('data.json', updateData, 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('JSON file has been updated.');
+  });
 })
 
 app.listen(port, () => {
